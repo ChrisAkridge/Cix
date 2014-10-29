d3c35b98edfb08eed11edaa4eaf364fe31d0c7ed
Chris Akridge <cakridge2@gmail.com>
2014-10-29 18:12:10 -0400

    Starting the AST element classes

---
+++ Cix/Cix/Cix/AST/DataType.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cix.AST
+{
+	public sealed class DataType
+	{
+		public string TypeName { get; private set; }
+		public int PointerLevel { get; private set; }
+
+		public DataType(string typeName, int pointerLevel)
+		{
+			if (string.IsNullOrEmpty(typeName))
+			{
+				throw new ArgumentException("Invalid name for data type.");
+			}
+
+			if (pointerLevel < 0)
+			{
+				throw new ArgumentException(string.Format("Invalid pointer level {0}", pointerLevel));
+			}
+
+			this.TypeName = typeName;
+			this.PointerLevel = pointerLevel;
+		}
+
+		public Type GetBCLType()
+		{
+			if (this.PointerLevel != 0)
+			{
+				throw new InvalidOperationException("This type is a pointer type; for type safety Cix cannot return the analogous BCL type.");
+			}
+
+			switch (this.TypeName)
+			{
+				case "char":
+					return typeof(byte);
+				case "schar":
+					return typeof(sbyte);
+				case "short":
+					return typeof(short);
+				case "ushort":
+					return typeof(ushort);
+				case "int":
+					return typeof(int);
+				case "uint":
+					return typeof(uint);
+				case "long":
+					return typeof(long);
+				case "ulong":
+					return typeof(ulong);
+				case "float":
+					return typeof(float);
+				case "double":
+					return typeof(double);
+				case "void":
+					return typeof(void);
+				default:
+					throw new InvalidOperationException(string.Format("The Cix type {0} does not have an analogous BCL type.", this.TypeName));
+			}
+		}
+	}
+}
---
+++ Cix/Cix/Cix/AST/FunctionArgument.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cix.AST
+{
+	public sealed class FunctionArgument : Element
+	{
+		public DataType Type { get; private set; }
+		public string ArgumentName { get; private set; }
+
+		public FunctionArgument(DataType type, string argumentName)
+		{
+			if (string.IsNullOrEmpty(argumentName))
+			{
+				throw new ArgumentException("The name of a function argument cannot be null or empty.");
+			}
+
+			this.Type = type;
+			this.ArgumentName = argumentName;
+		}
+	}
+}
