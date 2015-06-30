4d337adb39fec93139fffd44f1afc1d202707f70
Chris Akridge <cakridge2@gmail.com>
2015-06-30 15:19:38 -0400

    Two more AST types

---
+++ Cix/Cix/Cix/AST/VariableDeclarationWithInitialization.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cix.AST
+{
+	public sealed class VariableDeclarationWithInitialization : Element
+	{
+		public DataType VariableType { get; private set; }
+		public string VariableName { get; private set; }
+
+		// Notes: in, say, int i = 4 + y, the OpAssignment operator is implied;
+		// it should not be included in the assignment expression
+		public Expression AssignmentExpression { get; private set; }
+
+		public VariableDeclarationWithInitialization(DataType variableType, string variableName, Expression assignmentExpression)
+		{
+			this.VariableType = variableType;
+			this.VariableName = variableName;
+			this.AssignmentExpression = assignmentExpression;
+		}
+	}
+}
